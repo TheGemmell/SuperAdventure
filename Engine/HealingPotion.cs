f1e157aebef7dd07938bb6f5a142164e97d0674c
Chris <fablekey@gmail.com>
2022-09-21 18:05:30 +1000

    added basic classes for Items/Quests/etc.

---
+++ Engine/HealingPotion.cs
@@ -0,0 +1,10 @@
+namespace Engine
+{
+    public class HealingPotion
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string NamePlural { get; set; }
+        public int HealAmount { get; set; }
+    }
+}
