6bb1aa7059b2b8b884925d696d92de8a3e568b56
Chris <fablekey@gmail.com>
2022-09-27 12:24:47 +1000

    added more classes

--- Engine/Monster.cs
+++ Engine/Monster.cs
@@ -1,7 +1,11 @@
 namespace Engine
 {
-    public class Monster
+    public class Monster : LivingCreature
     {
-        
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int MaximumDamage { get; set; }
+        public int RewardExperiencePoints { get; set; }
+        public int RewardGold { get; set; }
     }
 }
